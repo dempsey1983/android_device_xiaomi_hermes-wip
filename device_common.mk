47974b8e0fc180af2e461289988f045af34a97f8
Lok Jian Ming <jianminglok@hotmail.com>
2016-04-28 20:11:20 +0800

    Update device_common.mk

--- device_common.mk
+++ device_common.mk
@@ -4,11 +4,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/meizu/arale/arale-vendor.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/hermes/hermes-vendor-blobs.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/meizu/arale/overlay
+DEVICE_PACKAGE_OVERLAYS += device/xiaomi/hermes/overlay
 
-LOCAL_PATH := device/meizu/arale
+LOCAL_PATH := device/xiaomi/hermes
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 else
@@ -61,6 +62,11 @@ PRODUCT_PACKAGES += \
     audio_policy.default \
     libtinycompress \
     libtinyxml \
+PRODUCT_COPY_FILES += \
+    device/xiaomi/hermes/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    device/xiaomi/hermes/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/xiaomi/hermes/configs/audio_device.xml:system/etc/audio_device.xml \
+    device/xiaomi/hermes/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # this can't be compiled right now due to ABI mismatches (MTK_Tfa98xx_SetEQ),
 # and I don't feel like compiling the whole audio HAL from the source...
@@ -77,23 +83,28 @@ PRODUCT_PACKAGES += \
     libdrmmtkutil \
 
 # Wifi
-PRODUCT_PACKAGES += \
+ PRODUCT_PACKAGES += \
+    lib_driver_cmd_mt66xx \
+    libwifi-hal-mt66xx \
+    wifi_hal \
     libwpa_client \
     hostapd \
+    hostapd_cli \
     dhcpcd.conf \
     wpa_supplicant \
     wpa_supplicant.conf
+    
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny
-
+    device/xiaomi/hermes/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+    device/xiaomi/hermes/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    device/xiaomi/hermes/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny
+    
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
     audio.a2dp.default
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/bt_did.conf:system/etc/bluetooth/bt_did.conf
+    device/xiaomi/hermes/configs/bt_did.conf:system/etc/bluetooth/bt_did.conf
 
 # RIL
 PRODUCT_PACKAGES += \
@@ -120,12 +131,6 @@ PRODUCT_PACKAGES += \
     mtkmal \
     charon starter stroke ipsec \
     libcharon libhydra libstrongswan libsimaka \
-
-# wifi offload service common library
-#PRODUCT_PACKAGES += wfo-common  # this is pulled in by ImsService
-#ifeq ($(strip $(MTK_EPDG_SUPPORT)),yes)
-PRODUCT_PACKAGES += WfoService libwfo_jni
-#endif
 
 # IMS and VoLTE feature
 #ifeq ($(strip $(MTK_IMS_SUPPORT)),yes)
@@ -166,66 +171,95 @@ PRODUCT_PACKAGES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl \
-    $(LOCAL_PATH)/configs/mtk-tpd.kl:system/usr/keylayout/mtk-tpd.kl
+     device/xiaomi/hermes/configs/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl \
+     device/xiaomi/hermes/configs/ACCDET.kl:system/usr/keylayout/ACCDET.kl\
+     device/xiaomi/hermes/configs/AVRCP.kl:system/usr/keylayout/AVRCP.kl   
 
 # Thermal
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/configs/.ht120.mtc:system/etc/.tp/.ht120.mtc \
-     $(LOCAL_PATH)/configs/thermal.conf:system/etc/.tp/thermal.conf \
-     $(LOCAL_PATH)/configs/thermal.off.conf:system/etc/.tp/thermal.off.conf \
-     $(LOCAL_PATH)/configs/thermal.high.conf:system/etc/.tp/thermal.high.conf \
-     $(LOCAL_PATH)/configs/thermal.low.conf:system/etc/.tp/thermal.low.conf \
-     $(LOCAL_PATH)/configs/thermal.mid.conf:system/etc/.tp/thermal.mid.conf
+     device/xiaomi/hermes/configs/.ht120.mtc:system/etc/.tp/.ht120.mtc \
+     device/xiaomi/hermes/configs/thermal.conf:system/etc/.tp/thermal.conf \
+     device/xiaomi/hermes/configs/thermal.off.conf:system/etc/.tp/thermal.off.conf \
+     device/xiaomi/hermes/configs/.thermal_policy_00:system/etc/.tp/.thermal_policy_00 \
+     device/xiaomi/hermes/configs/.thermal_policy_01:system/etc/.tp/.thermal_policy_01 
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/root/enableswap.sh:root/enableswap.sh \
-    $(LOCAL_PATH)/root/fstab.mt6595:root/fstab.mt6595 \
-    $(LOCAL_PATH)/root/twrp.fstab:recovery/root/etc/twrp.fstab \
-    $(LOCAL_PATH)/root/init.mt6595.rc:root/init.mt6595.rc \
-    $(LOCAL_PATH)/root/init.mt6595.usb.rc:root/init.mt6595.usb.rc \
-    $(LOCAL_PATH)/root/init.recovery.mt6595.rc:root/init.recovery.mt6595.rc \
-    $(LOCAL_PATH)/root/init.aee.rc:root/init.aee.rc \
-    $(LOCAL_PATH)/root/init.modem.rc:root/init.modem.rc \
-    $(LOCAL_PATH)/root/init.volte.rc:root/init.volte.rc \
-    $(LOCAL_PATH)/root/init.epdg.rc:root/init.epdg.rc \
-    $(LOCAL_PATH)/root/init.mal.rc:root/init.mal.rc \
-    $(LOCAL_PATH)/root/init.wfca.rc:root/init.wfca.rc \
-    $(LOCAL_PATH)/root/init.project.rc:root/init.project.rc \
-    $(LOCAL_PATH)/root/init.ssd.rc:root/init.ssd.rc \
-    $(LOCAL_PATH)/root/init.xlog.rc:root/init.xlog.rc \
-    $(LOCAL_PATH)/root/ueventd.mt6595.rc:root/ueventd.mt6595.rc \
-    $(LOCAL_PATH)/root/sbin/busybox:root/sbin/busybox \
-    $(LOCAL_KERNEL):kernel
+    $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
+    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/rootdir/fstab.charger:root/fstab.charger\
+    $(LOCAL_PATH)/rootdir/fstab.mt6795:root/fstab.mt6795 \
+    $(LOCAL_PATH)/rootdir/init.aee.rc:root/init.aee.rc \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6795.rc:root/init.mt6795.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6795.usb.rc:root/init.mt6795.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/rootdir/init.rc:root/init.rc \
+    $(LOCAL_PATH)/rootdir/init.ssd.rc:root/init.ssd.rc \
+    $(LOCAL_PATH)/rootdir/init.xlog.rc:root/init.xlog.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+    $(LOCAL_PATH)/rootdir/init:root/init \
+    $(LOCAL_PATH)/rootdir/init.hermes.power.rc:root/init.hermes.power.rc \
+    $(LOCAL_PATH)/rootdir/init.storage.rc:root/init.storage.rc \
+    $(LOCAL_PATH)/rootdir/init.cm.rc:root/init.cm.rc \
 
-# Permissions
+# These are the hardware-specific features
 PRODUCT_COPY_FILES += \
+    device/xiaomi/hermes/configs/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
+    device/xiaomi/hermes/configs/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    device/xiaomi/hermes/configs/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.manual_sensor.xml:system/etc/permissions/android.hardware.camera.manual_sensor.xml \
-    frameworks/native/data/etc/android.hardware.camera.manual_postprocessing.xml:system/etc/permissions/android.hardware.camera.manual_postprocessing.xml \
-    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+# Media	
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+PRODUCT_COPY_FILES += \
+	device/xiaomi/hermes/configs/media_codecs.xml:system/etc/media_codecs.xml \
+	device/xiaomi/hermes/configs/media_codecs.xml:system/etc/permissions/media_codecs.xml \
+	device/xiaomi/hermes/configs/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Charger
+PRODUCT_PACKAGES += \
+      charger_res_images
 
 # USB
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp \
-    ro.hardware=mt6595 \
+    ro.hardware=mt6795 \
 
 # NFC
 #PRODUCT_PACKAGES += \
@@ -237,9 +271,59 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 #    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
 #    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
+# Ifaddrs
+PRODUCT_PACKAGES += \
+    libifaddrs
+
 PRODUCT_PACKAGES += \
     Torch
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
+     ro.adb.secure=0 \
+     ro.secure=1 \
+     ro.allow.mock.location=1 \
+     ro.debuggable=1 \
+     ro.zygote=zygote64_32 \
+     camera.disable_zsl_mode=1 \
+     ro.mount.fs=EXT4 \
+     persist.service.acm.enable=0 \
+     persist.sys.usb.config=mtp,mass_storage \
+     persist.sys.timezone=Asia/Shanghai
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    ro.telephony.ril_class=MediaTekRIL \
+    ro.telephony.ril.config=fakeiccid  \
+    persist.call_recording.enabled=true \
+    persist.call_recording.src=4 \
+    persist.debug.wfd.enable=1
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory
+
+ PRODUCT_PACKAGES += \
+    libnl_2 \
+    libtinyxml
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    e2fsck \
+    fibmap.f2fs \
+    fsck.f2fs \
+    mkfs.f2fs \
+    make_ext4fs \
+    resize2fs \
+    setup_fs \
+    ext4_resize \
+    resize_ext4 \
+    superumount 
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=8
+
+# Dalvik/HWUI
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 $(call inherit-product, build/target/product/full.mk)
 
