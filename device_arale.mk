0bffc97c9fd15cd45d7be25d40981e5a591c6b81
Lok Jian Ming <jianminglok@hotmail.com>
2016-04-28 20:11:13 +0800

    Update device_arale.mk

--- device_arale.mk
+++ device_arale.mk
@@ -1,8 +1,30 @@
--include device/meizu/arale/device_common.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_arale
-PRODUCT_DEVICE := arale
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/xiaomi/hermes/device.mk)
 
-# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
+# Release name
+PRODUCT_RELEASE_NAME := xiaomi hermes
+
+EXTENDED_FONT_FOOTPRINT := true
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.timezone=Asia/Shanghai
+
+PRODUCT_DEVICE := hermes
+PRODUCT_NAME := full_hermes
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi Note 2
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Boot animation
+TARGET_SCREEN_HEIGHT      := 1920
+TARGET_SCREEN_WIDTH       := 1080
+TARGET_BOOTANIMATION_NAME := 1080
+
+PRODUCT_DEFAULT_LANGUAGE := en
+PRODUCT_DEFAULT_REGION   := US
