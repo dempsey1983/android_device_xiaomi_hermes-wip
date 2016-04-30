3981f5e60fdef0e930bda4eed6e4cf3f6ea380b1
Lok Jian Ming <jianminglok@hotmail.com>
2016-04-30 21:44:46 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -180,27 +180,35 @@ PRODUCT_COPY_FILES += \
      device/xiaomi/hermes/configs/.ht120.mtc:system/etc/.tp/.ht120.mtc \
      device/xiaomi/hermes/configs/thermal.conf:system/etc/.tp/thermal.conf \
      device/xiaomi/hermes/configs/thermal.off.conf:system/etc/.tp/thermal.off.conf \
+     device/xiaomi/hermes/configs/.thermal_policy_00:system/etc/.tp/.thermal_policy_00 \
+     device/xiaomi/hermes/configs/.thermal_policy_01:system/etc/.tp/.thermal_policy_01 
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/root/enableswap.sh:root/enableswap.sh \
-    $(LOCAL_PATH)/root/fstab.mt6595:root/fstab.mt6595 \
-    $(LOCAL_PATH)/root/twrp.fstab:recovery/root/etc/twrp.fstab \
-    $(LOCAL_PATH)/root/init.mt6595.rc:root/init.mt6595.rc \
-    $(LOCAL_PATH)/root/init.mt6595.usb.rc:root/init.mt6595.usb.rc \
-    $(LOCAL_PATH)/root/init.recovery.mt6595.rc:root/init.recovery.mt6595.rc \
-    $(LOCAL_PATH)/root/init.aee.rc:root/init.aee.rc \
-    $(LOCAL_PATH)/root/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
+    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/rootdir/fstab.charger:root/fstab.charger\
+    $(LOCAL_PATH)/rootdir/fstab.mt6795:root/fstab.mt6795 \
+    $(LOCAL_PATH)/rootdir/init.aee.rc:root/init.aee.rc \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6795.rc:root/init.mt6795.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6795.usb.rc:root/init.mt6795.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/rootdir/init.rc:root/init.rc \
+    $(LOCAL_PATH)/rootdir/init.ssd.rc:root/init.ssd.rc \
+    $(LOCAL_PATH)/rootdir/init.xlog.rc:root/init.xlog.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+    $(LOCAL_PATH)/rootdir/init:root/init \
+    $(LOCAL_PATH)/rootdir/init.hermes.power.rc:root/init.hermes.power.rc \
+    $(LOCAL_PATH)/rootdir/init.storage.rc:root/init.storage.rc \
+    $(LOCAL_PATH)/rootdir/init.cm.rc:root/init.cm.rc \
     $(LOCAL_PATH)/root/init.volte.rc:root/init.volte.rc \
     $(LOCAL_PATH)/root/init.epdg.rc:root/init.epdg.rc \
     $(LOCAL_PATH)/root/init.mal.rc:root/init.mal.rc \
     $(LOCAL_PATH)/root/init.wfca.rc:root/init.wfca.rc \
-    $(LOCAL_PATH)/root/init.project.rc:root/init.project.rc \
-    $(LOCAL_PATH)/root/init.ssd.rc:root/init.ssd.rc \
-    $(LOCAL_PATH)/root/init.xlog.rc:root/init.xlog.rc \
-    $(LOCAL_PATH)/root/ueventd.mt6595.rc:root/ueventd.mt6595.rc \
-    $(LOCAL_PATH)/root/sbin/busybox:root/sbin/busybox \
-    $(LOCAL_KERNEL):kernel
    
 #These are the hardware-specific features
 PRODUCT_COPY_FILES += \
